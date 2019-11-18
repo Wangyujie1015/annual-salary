d9cdf9ed43a487e4e47d77f2bf702c022cfaab3a
王宇杰 <1285589294@qq.com>
2019-11-18 18:03:55 +0800

    1.在屏幕上输出以下图案： *  ***  *****  *******  *********  *********** *************  ***********  *********  *******  *****  ***  *
    2.求出0～999之间的所有“水仙花数”并输出。
    “水仙花数”是指一个三位数，其各位数字的立方和确好等于该数本身，如；153＝1＋5＋3?，则153是一个“水仙花数”。
    
    /*
    在数论中，水仙花数（Narcissistic number）也称为自恋数、自幂数、阿姆斯壮数或阿姆斯特朗数（Armstrong number），是指一N位数，其各个数之N次方和等于该数。
    例如153、370、371及407就是三位数的水仙花数，其各个数之立方和等于该数：
    153 = 1^3 + 5^3 + 3^3。
    370 = 3^3 + 7^3 + 0^3。
    371 = 3^3 + 7^3 + 1^3。
    407 = 4^3 + 0^3 + 7^3。
    */
    
    3.
    求Sn=a+aa+aaa+aaaa+aaaaa的前5项之和，其中a是一个数字，
    例如：2+22+222+2222+22222

---
+++ Project2/Project2/zuoye.c
@@ -0,0 +1,49 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	//for (int a = 1; a <= 5; a++) {
+	//	for (int i = 1; i < a * 2; i++) {
+	//		printf("*");
+	//	}
+	//	printf("\n");
+	//}
+	//for (int a = 4; a >=1; a--) {
+	//	for (int i = 1; i < a * 2; i++) {
+	//		printf("*");
+	//	}
+	//	printf("\n");
+	//}
+2
+	//for (int i = 100; i <= 999; i++) {
+	//	int a = i / 100;
+	//	int b = i / 10 % 10;
+	//	int c = i % 10;
+	//	if (i == (a*a*a + b * b*b + c * c*c)) {
+	//		printf("水仙花书有:%d\n", i);
+	//	}
+
+	//}
+3
+//{
+//	int Sn = 0;
+//	int sum = 0;
+//	int i, a;
+//	printf("请输入一个0到9的正整数:");
+//	scanf("%d/n", &a);
+//	for (i = 0; i < 5; i++)
+//	{
+//		sum = sum * 10 + a;
+//		Sn = Sn + sum;
+//	}
+//	printf("%d", Sn);
+//
+
+	system("pause");
+	return 0;
+}
+
+
+
