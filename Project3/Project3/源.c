4e42810453ae8c28aa77d1dc5e93e97a8b477cf4
王宇杰 <1285589294@qq.com>
2019-11-20 01:16:30 +0800

    无

---
+++ Project3/Project3/源.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	//int number = 0;
+	//printf("-----------\n");
+	//printf("开始游戏\n");
+	//printf("-----------\n");
+	//srand(time(NULL));
+	//int num = rand()%100+1;
+	//while (num != number) {
+	//printf("请输入一个 1-100的数字\n");
+	//scanf("%d", &number);
+	//if(number == num){ 
+	//	printf("你猜对了");
+	//	break;
+
+	//}
+
+	//	if (number > num) {
+	//		printf("你猜大了\n");
+	//	}
+	//	else
+	//		printf("你猜小了\n");
+	//}
+	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int lift=arr[0];
+	//int right=sizeof(arr)/sizeof(arr[0]);
+	//printf("请输入1-10的数字\n");
+	//int num;
+	//scanf("%d", &num);
+	//while(lift == right); {
+	//	int mid = (lift + right) / 2;
+	//	if (num == mid) {
+	//		printf("找到了\n");
+	//		printf("%d\n", mid - 1);  //在%d前面加上其他东西程序就崩溃
+	//		
+	//	}
+	//	else if (mid > num) {
+	//		right = mid - 1;
+	//	}
+	//	else {
+	//		lift = mid + 1;
+	//	}
+	//}
+	//printf("请设置初始密码\n");
+	//char passwold;
+	//scanf("%s", &passwold);
+	//for (int i = 1; i <= 3; i++) {
+	//	printf("请输入密码\n");
+	//	char num;
+	//	scanf("%s", &num);
+	//	if (num == passwold) {
+	//		printf("密码正确\n");
+	//		break;
+	//	}
+	//	else {
+	//		printf("密码错误\n");
+	//	}
+	//}
+	//while (1) {
+	//	char number;
+	//	scanf("%c", &number);
+
+	//	if (number >= 65 && number <= 90) {
+	//		printf("%c\n", number + 32);
+	//	}
+	//	if (number >= 'a'&&number <= 'z') {
+	//		printf("%c\n", number - 32);
+	//	}
+
+	//}
+
+		system("pause");
+	return 0;
+}
+
+
+
