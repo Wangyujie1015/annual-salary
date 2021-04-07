43250e062eb45c0b700efe495e76d48c5b0e7711
王宇杰 <1285589294@qq.com>
2021-04-07 22:28:03 +0800

    20210407.exe

---
+++ 20210407/20210407/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_DEPRECATE 1
+#include<stdio.h>
+#include<assert.h>
+//
+//char* my_strcpy(char*dest, const char*src)
+//{
+//	//printf("hehe");
+//	//return NULL;
+//	char* ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	
+//
+//	//printf("%p\n", &my_strcpy);
+//	char arr1[20] = { 0 };
+//	char arr2[] = "woaini";
+//
+//	char* (*pf)(char*, const char*) = &my_strcpy;
+//	(*pf)(arr1,arr2);
+//	printf("%s\n", arr1);
+//
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
