59c82ed8dd4c30de55d0dc17bb529b75fc472d25
wangyujie <1285589294@qq.com>
2021-04-20 22:38:04 +0800

    20210420

---
+++ 20210420/test.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	for (int a = 0; a <= 5; a++)
+	{
+		for (int b = 0; b <= 5; b++)
+		{
+			for (int c = 0; c <= 5; c++)
+			{
+				for (int d = 0; d <= 5; d++)
+				{
+					for (int e = 0; e <= 5; e++)
+					{
+						if (
+							(a==1)
+							)
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
