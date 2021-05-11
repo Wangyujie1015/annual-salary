28360185623f8d9e26732821ad34f1a27f66c975
wangyujie <1285589294@qq.com>
2021-05-11 22:44:52 +0800

    0511

---
+++ 20210511/test.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+
+//char* my_strstr(const char* str1, const char* str2)
+//{
+//	const char* s1 = str1;
+//	const char* s2 = str2;
+//	const char* cp = str1;
+//	
+//	if (*s2 == '\0')
+//	{
+//		return (char*)str1;
+//	}
+//	
+//	while (*cp)
+//	{
+//		s1 = cp;
+//		s2 = str2;
+//		
+//		while (*s1 && *s2 && *s1 == *s2)
+//		{
+//			s1++;
+//			s2++;
+//		}
+//		
+//		if (*s2 == '\0')
+//		{
+//			return (char*)cp;
+//		}
+//		cp++; 
+//	}
+//	return NULL;
+//}
+//
+//
+//int main()
+//{
+//	char* arr1 = "abcdef";
+//	char* arr2 = "cdef";
+//	char* ret = my_strstr(arr1, arr2);
+//	
+//	printf("%s\n",ret );
+//	return 0;
+//}
+
+
+
+
+
+
+int main()
+{
+	
+	return 0;
+}
+
