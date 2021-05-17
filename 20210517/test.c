b155193da2499b5cf96145b625ef4399bda7bbd3
wangyujie <1285589294@qq.com>
2021-05-17 23:31:17 +0800

    内存操作函数

---
+++ 20210517/test.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+//学生类型
+/*struct Stu
+{
+	char name[20];
+	short age;
+	char sex[5];
+}s1,s2*/;//全局变量
+
+struct Stu
+{
+	char name[20];
+	short age;
+	char sex[5];
+};
+
+int main()
+{
+
+	return 0;
+}
